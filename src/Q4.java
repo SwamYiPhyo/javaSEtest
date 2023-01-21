3429713ebd040cd372bc5419da6ff4faf4189024
Swam Yi Phyo <6531503187@lamduan.mfu.ac.th>
2023-01-21 22:36:25 +0700

    first commit

---
+++ src/Q4.java
@@ -0,0 +1,42 @@
+interface AB
+{
+    void dosth();
+}
+class _A implements AB
+{
+    @Override
+    public void dosth() {
+        System.out.println("Do laundaries");
+    }
+}
+class _B implements AB
+{
+    @Override
+    public void dosth() {
+        System.out.println("Sweep the floor");
+    }
+}
+class _C
+{
+    AB ab;
+    _C(AB a)
+    {
+        ab = a;
+    }
+    void call()
+    {
+        ab.dosth();
+    }
+}
+public class Q3 {
+    public static void main(String[] args) {
+        _A a = new _A();
+        _C c = new _C(a);
+        c.call();
+
+        _B b = new _B();
+        _C c1 = new _C(b);
+        c1.call();
+
+    }
+}
